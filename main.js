7adc2162b62262d3f7e90b7c5b9cd4bf0e9d6b75
Farah <farah.hsh.12@gmail.com>
2023-08-09 21:06:19 +0300

    day 4 in progress

---
+++ main.js
@@ -0,0 +1,82 @@
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// day4 introduction to nonprimitive datatype
+
+
+// nonprimitive includes: objects, array 
+
+
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// 1  object 
+
+
+let testobj= {};
+console.log(typeof testobj); 
+
+let textobject={
+// key:value
+    nr:2,
+    str:"text",
+
+}
+
+// to delete a key:
+// delete textobject.str
+
+console.log(typeof textobject.nr);
+console.log( textobject.nr);
+
+console.log(typeof textobject.str);
+console.log( textobject.str);
+
+
+// end of object
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+// challenge
+
+let object0={
+    
+    name1: "sara",
+    surname1:"mohamad",
+    age1:22,
+    email1:"SaraMohamad@teleworm.us",
+
+}
+
+console.log( object0.name1);
+console.log( object0.surname1);
+console.log( object0.age1);
+console.log( object0.email1);
+
+let object2={
+
+    name2: "salman",
+    surname2:"ahmed",
+    age2:20,
+    email2:"salmanahmed@dayrep.com",
+
+}
+
+console.log( object2.name2);
+console.log( object2.surname2);
+console.log( object2.age2);
+console.log( object2.email2);
+
+
+// end of challenge
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// 2   array
+
+
+
+// end
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
